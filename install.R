88c4a9c659dd05f788e515ef4dfcbbfe8bd255e6
Pierre Poulain <pierre.poulain@cupnet.net>
2020-04-09 14:44:07 +0200

    Add Shiny packages

--- install.R
+++ install.R
@@ -1,2 +1,10 @@
+# Data analysis
 install.packages("tidyverse")
+# R Markdown documents converter
 install.packages("rmarkdown")
+# HTTP tools
+install.packages("httr")
+# Shiny
+install.packages("shinydashboard")
+# Interactive maps
+install.packages('leaflet')
